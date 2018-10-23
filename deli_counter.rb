4eb73b7e70532cd49ca75a16edadde8a3bfa0875
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-23 05:34:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  string = "The line is currently:"
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name, index|
+    string << " #{index+1}. #{name}"
+  end
+  puts string
+end
+end
+
+def take_a_number(katz_deli, name)
+  index = katz_deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{index} in line."
+end
