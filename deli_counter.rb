b1cbd64ce148109f733753131377bc0d87e4f157
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-23 05:36:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,6 @@ end
 end
 
 def take_a_number(katz_deli, name)
-  index = katz_deli.index(name)
-  puts "Welcome, #{name}. You are number #{index} in line."
+  position = katz_deli.index(name)
+  puts "Welcome, #{name}. You are number #{position} in line."
 end
